4d2e7b67737291ca06bfb081cf759d636b3e5770
Greg Crossfield <greg@lightshipworks.com>
2017-04-08 10:58:43 -0700

    push starter kit to sdj

--- android/app/src/main/java/com/smoothie/MainActivity.java
+++ android/app/src/main/java/com/smoothie/MainActivity.java
@@ -1,4 +1,4 @@
-package com.starterkit;
+package com.smoothie;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "StarterKit";
+        return "Smoothie";
     }
 }
--- android/app/src/main/java/com/smoothie/MainApplication.java
+++ android/app/src/main/java/com/smoothie/MainApplication.java
@@ -1,4 +1,4 @@
-package com.starterkit;
+package com.smoothie;
 
 import android.app.Application;
 import android.util.Log;
